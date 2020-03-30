183d3ab14adf8747332090ba8b6cfbb1b5b0803d
onlyil <lin.cao@beibei.com>
2020-03-30 21:50:28 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: lin.cao
+ * @Date: 2020-03-28 21:49:26
+ * @LastEditTime: 2020-03-29 10:57:09
+ * @LastEditors: lin.cao
+ * @Description: 
+ * @FilePath: /webpack-babel-learn/src/index.js
+ */
+// class WeEvent {
+//   constructor(name) {
+//     this.name = name
+//   }
+// }
+
+// const af = async () => {
+//   await Promise.resolve(1)
+//   return 2
+// }
+
+const isHas = [1, 2, 3].includes(2)
+console.log(isHas)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: lin.cao
+ * @Date: 2020-03-29 09:49:37
+ * @LastEditTime: 2020-03-29 11:28:14
+ * @LastEditors: lin.cao
+ * @Description: 
+ * @FilePath: /webpack-babel-learn/webpack.config.js
+ */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'cheap-module-eval-source-map' : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    })
+  ]
+}
