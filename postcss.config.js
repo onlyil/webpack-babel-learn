7cf291b763d73ae6c8cb9c132b33e1e1d91b43a4
Onlyil <lifedamara@163.com>
2020-05-14 17:43:20 +0800

    Update: postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,5 @@
-/*
- * @Author: lin.cao
- * @Date: 2020-04-02 22:50:23
- * @LastEditTime: 2020-04-02 23:01:52
- * @LastEditors: lin.cao
- * @Description: 
- * @FilePath: /webpack-babel-learn/postcss.config.js
- */
 module.exports = {
-  // parser: 'sugarss',
-  plugins: {
-    'autoprefixer': {},
-    // 'postcss-import': {},
-    // 'postcss-preset-env': {},
-    // 'cssnano': {}
-  }
+  plugins: [
+    require('autoprefixer'),
+  ]
 }
