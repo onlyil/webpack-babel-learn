0291da05e9e814710bbecaf63cc93be2014e7c2c
Onlyil <lifedamara@163.com>
2020-05-14 18:41:50 +0800

    Update: css 提取、压缩

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /*
  * @Author: lin.cao
  * @Date: 2020-03-28 21:49:26
- * @LastEditTime: 2020-04-10 18:53:15
+ * @LastEditTime: 2020-05-14 18:40:18
  * @LastEditors: lin.cao
  * @Description: 
  * @FilePath: /webpack-babel-learn/src/index.js
  */
 import cssStyle from './style/css/index.css'
 import lessStyle from './style/less/index.less'
-import parser from './parser'
+// import parser from './parser'
 
 // class WeEvent {
 //   constructor(name) {
@@ -21,4 +21,36 @@ import parser from './parser'
 //   return 2
 // }
 
-const isHas = [1, 2, 3].includes(2)
+// const isHas = [1, 2, 3].includes(2)
+
+// function SuperType() {
+//     this.property = true
+// }
+
+// SuperType.prototype.getSuperValue = function () {
+//     return this.property
+// }
+
+// function SubType() {
+//     this.subProperty = false
+// }
+
+// SubType.prototype = new SuperType()
+
+// SubType.prototype.getSubValue = function () {
+//     return this.subProperty
+// }
+
+// const instance = new SubType()
+
+// console.log(instance, SubType.prototype, SubType.prototype.constructor)
+// console.log(instance.__proto__ === SubType.prototype)
+
+// function SuperType() {
+//     this.property = true
+// }
+// const ins = new SuperType()
+
+// console.log(SuperType)
+// console.log(SuperType.prototype)
+// console.log(Object.getPrototypeOf(ins).constructor === SuperType)
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
-/*
- * @Author: lin.cao
- * @Date: 2020-03-29 09:49:37
- * @LastEditTime: 2020-04-02 23:13:11
- * @LastEditors: lin.cao
- * @Description: 
- * @FilePath: /webpack-babel-learn/webpack.config.js
- */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
-  devtool: isDev ? 'cheap-module-eval-source-map' : 'source-map',
+  devtool: isDev ? 'cheap-module-eval-source-map' : 'none',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: isDev ? '[name].js' : '[name].[chunkhash:8].js',
+  },
   module: {
     rules: [
       {
@@ -22,17 +25,22 @@ module.exports = {
       {
         test: /\.(css|less)$/,
         use: [
-          'style-loader',
+          isDev ? 'style-loader': MiniCssExtractPlugin.loader,
           { loader: 'css-loader', options: { sourceMap: isDev } },
-          { loader: 'postcss-loader', options: { sourceMap: isDev } },
+          { loader: 'postcss-loader', options: { sourceMap: isDev ? 'inline' : false } },
           { loader: 'less-loader', options: { sourceMap: isDev } },
         ]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'index.html',
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDev ? '[name].css' : '[name].[contenthash:8].css',
+    }),
+    new OptimizeCssAssetsWebpackPlugin(),
   ]
 }
