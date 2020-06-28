829019a9eeeb925f0096e1867726484fef8d81fd
onlyil <lin.cao@beibei.com>
2020-06-28 16:14:52 +0800

    feat: generator 实现

--- src/learn/async-await-generator.js
+++ src/learn/async-await-generator.js
@@ -49,3 +49,60 @@ console.log(result)
 setTimeout(() => {
     console.log(result)
 }, 1000);
+
+// generator 实现：用 switch case 将代码阶段分割，定义context储存函数上下文，定义 next 方法
+// 1. gen$(_context) 由 yield 分割生成器代码而来，主要是 switch case
+// 2. context 用于储存函数执行上下文
+// 3. invoke 方法定义 next ，用于执行 gen$() 来跳到下一步
+
+// 生成器函数根据yield语句将代码分割为switch-case块，后续通过切换_context.prev和_context.next来分别执行各个case
+function gen$(_context) {
+    while (1) {
+        switch (_context.prev = _context.next) {
+        case0:
+        _context.next = 2;
+        return 'result1';
+
+        case2:
+        _context.next = 4;
+        return 'result2';
+
+        case4:
+        _context.next = 6;
+        return 'result3';
+
+        case6:
+        case "end":
+        return _context.stop();
+    }
+}
+
+// 低配版context
+var context = {
+    next: 0,
+    prev: 0,
+    done: false,
+    stop: function stop() {
+        this.done = true
+    }
+}
+
+// 低配版invoke
+let gen = function () {
+    return {
+        next: function () {
+            value = context.done ? undefined : gen$(context)
+            done = context.done
+            return {
+                value,
+                done
+            }
+        }
+    }
+}
+
+var g = gen()
+g.next()  // {value: "result1", done: false}
+g.next()  // {value: "result2", done: false}
+g.next()  // {value: "result3", done: false}
+g.next()  // {value: undefined, done: true}
